df63ae513b6e767d0a7069147134260ad51c1ff0
Тимур Белов <timurbl574@gmail.com>
2021-05-21 18:34:48 +0300

    lesson 19 complete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 90;
-
   return (
     <div className="App">
       <Navbar />
